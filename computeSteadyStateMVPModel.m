cb95fe33067b2d7c15c1ca143ca4753408d6acc6
MarianaDTU <s201205@student.dtu.dk>
2022-05-29 11:25:12 +0200

    e

--- computeSteadyStateMVPModel.m
+++ computeSteadyStateMVPModel.m
@@ -1,28 +1,42 @@
 function  [xs, us, flag] = computeSteadyStateMVPModel(t, p, Gs)
-%
-% COMPUTESTEADYSTATEMVPMODEL Compute the steady state of the Medtronic
-% Virtual Patient (MVP) model.
-%
+% 
+% computeSteadyStateMVPModel()
+% 
 % DESCRIPTION:
-% Solve a combination of the nonlinear right-hand side function and a set
-% of linear specification equations for the steady state of the Medtronic
-% Virtual Patient (MVP) model at given blood glucose concentration and zero
-% insulin and glucagon boli:
-%
-% Here, w = [x; u]. This set of nonlinear and linear equations are solved
-% using fsolve to a hardcoded tolerance of 10^(-10).
+% This function solves the equations from the mvpModelSteadyStateWrapper to
+% find the steady state vector and the manipulated inputs at steady state. 
+% It uses the function fsolve in matlab to solve the euqations when sat to 0.
+% To find an appropriate initial guess it uses the naturalMVPmodel.
 % 
 % INPUT:
 %   t  - time
-%   p  - a vector parameters               (dimension: 10)
-%   Gs - the steady state blood glucose concentration [mg/dL]
-%
-%
+%   p  - a vector parameters               
+%   Gs - the steady state blood glucose concentration 
+% 
 % OUTPUT:
-%   xs   - the steady state                    (dimension: 7)
-%   us   - the steady state manipulated inputs (dimension: 2)
+%   xs   - the steady state vector              
+%   us   - the steady state manipulated inputs 
 %   flag - the flag (integer) returned by fsolve
-
+% 
+% PROJECT:
+% Fagprojekt 2022
+% A diabetes case study - Meal detection
+%
+% GENEREL:
+% BSc                       : Mathematics and technology 
+% University                : The Technical University of Denmark (DTU)
+% Department                : Applied Mathematics and Computer Science 
+% 
+% AUTHORS:
+% Emma Victoria Lind
+% Mariana de Sá Madsen 
+% Mona Saleem
+% 
+% CONTACT INFORMATION
+% s201205@student.dtu.dk
+% s191159@student.dtu.dk
+% s204226@student.dtu.dk
+%
 
 % fsolve options
 opts = optimoptions('fsolve',       ...
@@ -30,10 +44,10 @@ opts = optimoptions('fsolve',       ...
     'Display',              'off');
 
 % The disturbance variables
-d = 0;
+d = 0; % No meal assumed
 
 % Initial guess of the states
-xs0 = naturalMVPmodel(p);
+xs0 = naturalMVPmodel(p); 
 
 % Initial guess of the manipulated inputs
 us0 = [0; 0];
@@ -41,14 +55,19 @@ us0 = [0; 0];
 % Initial guess
 ws0 = [xs0; us0];
 
-% Solve for the steady state
+% Solve for the steady state vector and manipulated inputs at steady state
 [ws, ~, flag] = fsolve(@mvpModelSteadyStateWrapper, ...
     ws0, opts, t, d, p, Gs);
 
-% Steady state
+
+% OUTPUT
+
+% Steady state vector
 xs = ws(1:end-2);
 
 % Steady state manipulated inputs
 us = ws(end-1:end);
 
 end
+
+
