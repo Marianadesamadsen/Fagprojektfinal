2167a7b1154f2dee10a7774fc5f54e2eec7d0397
MarianaDTU <s201205@student.dtu.dk>
2022-06-01 13:10:29 +0200

    e

--- Simnoise2_100patients.m
+++ Simnoise2_100patients.m
@@ -140,7 +140,7 @@ X=zeros(7,N+1,numpatients);
 % Looping over all patients 
 for p=1:numpatients
 
-[T(:,:,p), X(:,:,p)] = OpenLoopSimulation(x0(p,:)', tspan, U(:,:,p), D(:,:,p), pf(:,p), @MVPmodel2_noise, @EulerM2, Nk);
+[T(:,:,p), X(:,:,p)] = OpenLoopSimulation(x0(p,:)', tspan, U(:,:,p), D(:,:,p), pf(:,p), @MVPmodel, @EulerM2, Nk);
 
 end 
 
