dc3ae6cd31bf82cd3d6c1a1023e8ebd88b86cd63
MonaS42 <monas.a@hotmail.com>
2022-06-03 12:44:38 +0200

    test
    test

--- Data_indlaesning_csv.m
+++ Data_indlaesning_csv.m
@@ -3,7 +3,16 @@
 csvread('Control-IQ_Sample_Tconnect.csv', 2,2)
 
 opts.SelectedVariableNames = [1:5];
+
+opts.Sheet = 'CGM'
 readmatrix('Control-IQ_Sample_Tconnect.csv')
 
 %%
-data = readtable('Control-IQ_Sample_Tconnect.csv')
+data_temp = readtable('Control-IQ_Sample_Tconnect.csv');
+
+data = data_temp(1:779, 3);
+
+%% 
+importdata('Control-IQ_Sample_Tconnect.csv')
+
+
