e9255f2278792bd4b257532d31a35ebe5e454a68
MonaS42 <monas.a@hotmail.com>
2022-06-04 10:39:04 +0200

    datetime
    t

--- Data_indlaesning_csv.m
+++ Data_indlaesning_csv.m
@@ -18,9 +18,31 @@ data_temp = importdata('Control-IQ_Sample_Tconnect.csv');
 data = struct2cell(data_temp);
 
 % Convert to CGM data and date of time
-CGM_data = data{1}
+CGM_data = data{1};
 
-date_temp = data{2}
-date = date_temp(2:780, 4)
+date_temp = data{2};
+date = date_temp(2:780, 4);  % The date in cell aray
 
 
+DATE = regexprep(date, 'T', ' '); % Removes the T's in the dates and replaces with a space such that it gets the right format in datetime
+
+% We loop over the cell array and convert it to a string such that it gets
+% the right format for datetime
+for i = 1:length(DATE)
+    str = string(DATE{i});  
+    t(i)= datetime(str,'InputFormat','yyyy-MM-dd HH:mm:ss');
+end
+
+% Test med forskellige 'funktioner'
+% date_string = cellstr(date); % The date in cell aray
+%date_char = cell2mat(date) % The date in character array
+
+% for i = 1:length(DATE)
+%     str = 
+% d = '2018-06-25 11:23:37.712';
+% t = datetime(test,'InputFormat','yyyy-MM-dd HH:mm:ss.SSS')
+
+
+% %% EVENTUELT BRUG DEN HER NEDENSTÅENDE FOR DER BEHØVES MAN IKKE FJERNE T
+% !!!!!!
+% t = datetime(date_char,'InputFormat','uuuu-MM-dd HH:mm:ss''T''HH:mmXXX','TimeZone','UTC')
