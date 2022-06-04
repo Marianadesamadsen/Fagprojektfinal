7f02035c6ea76f4182edf436482fac04a6d75da2
MarianaDTU <s201205@student.dtu.dk>
2022-06-04 09:17:52 +0200

    e

--- brownianmotion2.m
+++ brownianmotion2.m
@@ -2,7 +2,7 @@ function W=brownianmotion2(Nk,tspan)
 
 tk=(tspan(end)-tspan(1))/Nk;
 
-randn('state',100) % set the state of randn. Det gør at vi får de samme
+%randn('state',100) % set the state of randn. Det gør at vi får de samme
 %random tal hver gang 
 
 dt = tk/Nk;
