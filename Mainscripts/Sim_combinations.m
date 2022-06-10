867203403b6001065a86dee694a75ec34bb75bac
MarianaDTU <s201205@student.dtu.dk>
2022-06-10 13:01:49 +0200

    e

--- Mainscripts/Sim_combinations.m
+++ Mainscripts/Sim_combinations.m
@@ -130,8 +130,8 @@ G = CGMsensor(X, p); % [mg/dL]
 
 % Range for gmin try outs 
 gmin1range = (125:5:135);
-gmin2range = (0.5:0.4:1.5);
-gmin3range = (0.5:0.4:1.5);
+gmin2range = (1:0.4:2);
+gmin3range = (1:0.4:2);
 
 % Computing the combinations using meshgrid
 [Gmin1,Gmin2,Gmin3] = meshgrid(gmin1range,gmin2range,gmin3range);
