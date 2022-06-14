264efa964c32b11bb4666db611ba4e748418b2b8
MonaS42 <monas.a@hotmail.com>
2022-06-14 19:35:12 +0200

    h

--- Mainscripts/RealData_xls.m
+++ Mainscripts/RealData_xls.m
@@ -50,7 +50,101 @@ end
 % Datetime for insulin and carb
 for i = 1:length(time_temp_insulin_carb)
     t2(i)= datetime(time_temp_insulin_carb{i},'InputFormat','dd/MM/yyyy HH:mm');
+    
 end
+
+% %% Test for data
+% 
+% date_test = (t2(70:85));
+% for i = 1:length(date_test)
+%     if isequal(date_test(i), date_test(i+1)) == 1
+%         date_test(i) = []
+%     end
+% end
+
+% %% ***** FUNGERER IKKE The non duplicated datetimes for insulin, no repeats of time****
+% time_CGM = unique(t);
+% time_insulin = unique(t2);
+% 
+% % The most measurements within almost the same time interval we have is
+% % from the CGM measurements
+% time_range = length(time_CGM);
+% % Adding 100 extra datetimes in each end of the time interval
+% extendFactor = 20;
+% % Insulin starts getting measured 2 minutes before CGM and finishes 1
+% % minute after CGM
+% t_1 = time_insulin(1);
+% t_2 = time_insulin(end);
+% 
+% time_vec = linspace(t_1, t_2, time_range *extendFactor);
+% 
+% % Adjusting the bolus insulin vector to match the time_vec
+% %vec_ratio = length(time_vec)/length(ubo);  % length ratio between time vector and bolus vector. Is gonna be used for indexing
+% %zero_ubo_vec = zeros(1,length(time_vec));  % allocating space for the extended bolus vector
+% 
+% idx_bolus = find(ubo);
+% 
+% zero_ubo_vec(1,length(time_vec));
+% 
+% for i = 1:length(idx_bolus)
+%     zero_ubo_vec(idx_bolus(i)*extendFactor) = ubo(idx_bolus(i));
+% end
+% 
+% ubo_vec = zero_ubo_vec;
+
+%% The non duplicated datetimes for insulin, no repeats of time
+%time_CGM = unique(t);       % Filtered time for CGM
+%time_insulin = unique(t2);  % Filtered time for insulin
+ 
+%t_start = min(time_CGM(1),time_insulin(1)); % Start time, the one out of the two measurements that start first
+%t_end = max(time_CGM(end), time_insulin(end)); % Finish time, the one out of the two measurements that finishes last
+% 
+% % timerange = max(length(time_CGM),length(time_insulin));
+% 
+% time_vec2 = [t_start, time_CGM, t_end];
+
+%% Plot over 3 days from january 2nd till end of January 4th
+t_start = min(t(1),t2(1)); % Start time, the one out of the two measurements that start first
+t_end = max(t(end), t2(end)); % Finish time, the one out of the two measurements that finishes last
+
+time_vec_CGM = [t_start, t, t_end];
+time_vec_insulin = [t_start, t2, t_end];
+
+time3days_CGM = time_vec_CGM(1:858+1);
+time3days_insulin = time_vec_insulin(1:482+1);
+
+CGM_3 = CGM(1:858);
+CGM_3(end+1) = 0;
+
+ubo_3 = ubo(1:482);
+ubo_3(end+1) = 0;
+
+uba_3 = uba(1:482);
+uba_3(end+1) = 0;
+
+figure (1)
+subplot(3,1,1)
+plot(time3days_CGM, CGM_3);
+ylabel({'Blood glucose concentration', '[mg/dL]'});
+xlabel('Time');
+title('Blood glucose concentration over time')
+
+
+subplot(3,1,2)
+stem(time3days_insulin, ubo_3)
+ylabel({'Bolus insulin', '[U]'}); 
+xlabel('Time');
+title('Bolus insulin flow rate')
+
+
+subplot(3,1,3)
+stairs(time3days_insulin, uba_3)
+ylabel({'Basal insulin', '[mU/min]'});
+xlabel('Time');
+title('Basal insulin flow rate')
+
+
+
 %% GRID - temp
 delta_G        = 15;                 % From article
 tau            = 6;                  % From the article
