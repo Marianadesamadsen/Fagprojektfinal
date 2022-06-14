b0eed3708571cd85b9b7ad9ad22b387320c8c9e9
MarianaDTU <s201205@student.dtu.dk>
2022-06-14 09:01:57 +0200

    e

--- Mainscripts/RealData_xls.m
+++ Mainscripts/RealData_xls.m
@@ -1,44 +1,52 @@
 %% Loading real clinical data from excel where data is stored in sheets
+
 clear all
 clc
+
 %% Adding the path 
+
 addpath(genpath(fullfile(pwd, '../Other')));
 
 CGM_sheet = readtable('Control-IQ_Sample_Diasend','Sheet','CGM');
 Insulin_carbs_sheet = readtable('Control-IQ_Sample_Diasend', 'Sheet','Insulin and Carbs');
 
-% Store the data in temporary arrays and the time in cell
-CGM = table2array(CGM_sheet(:,2));                  % Glucose concentration
-uba_temp = table2array(Insulin_carbs_sheet(:,2));   % Basal insulin
-ubo_temp = table2array(Insulin_carbs_sheet(:,4));   % Bolus insulin
-meal_temp = table2array(Insulin_carbs_sheet(:,8));  % Amount of carbs
+% Store the data in temporary arrays
+CGM                     = table2array(CGM_sheet(:,2));              % Glucose concentration
+uba_temp                = table2array(Insulin_carbs_sheet(:,2));    % Basal insulin
+ubo_temp                = table2array(Insulin_carbs_sheet(:,4));    % Bolus insulin
+meal_temp               = table2array(Insulin_carbs_sheet(:,8));    % Amount of carbs
 
-time_temp_CGM = table2cell(CGM_sheet(:,1));         % Time frame for glucose measurements
-time_temp_insulin_carb = table2cell(Insulin_carbs_sheet(:,1)); % Time frame for insulin and meal measurements
+% Store time in cell
+time_temp_CGM           = table2cell(CGM_sheet(:,1));               % Time frame for glucose measurements
+time_temp_insulin_carb  = table2cell(Insulin_carbs_sheet(:,1));     % Time frame for insulin and meal measurements
 
 %% Replacing the missing values NaN with zeros
 
 % Replacing all NaN values with 0
-uba_temp(isnan(uba_temp))=0;
-ubo_temp(isnan(ubo_temp))=0;
-meal_temp(isnan(meal_temp))=0;
+uba_temp(isnan(uba_temp))       = 0;
+ubo_temp(isnan(ubo_temp))       = 0;
+meal_temp(isnan(meal_temp))     = 0;
 
-uba = uba_temp;     % Filtered numerical values for basal insulin
-ubo = ubo_temp;     % Filtered numerical values for bolus insulin
-meal = meal_temp;   % Filtered numerical values for meals
+% Changning variables names after removing NAN with zeros
+uba     = uba_temp;             % Filtered numerical values for basal insulin
+ubo     = ubo_temp;             % Filtered numerical values for bolus insulin
+meal    = meal_temp;            % Filtered numerical values for meals
 
 %% Conversion factors
 
-mmolLiter2mgDl = 18; % Multiply with 18 for converting mmol/L to mg/dL
-h2min = 60;          % Convert from h to min
-min2h = 1/h2min;     % Convert from min to h 
-U2mU  = 1e3;         % Convert from U to mU 
-mU2U  = 1/U2mU;      % Convert from U to mU
+mmolLiter2mgDl  = 18;          % Multiply with 18 for converting mmol/L to mg/dL
+h2min           = 60;          % Convert from h to min
+min2h           = 1/h2min;     % Convert from min to h 
+U2mU            = 1e3;         % Convert from U to mU 
+mU2U            = 1/U2mU;      % Convert from U to mU
 
 %% Converting the measurements to correct units
+
 CGM = CGM * mmolLiter2mgDl;    % Glucose measurements are given in mmol/L and we convert to mg/dL
 uba = uba * U2mU*min2h;        % Insulin measurements are given in U/h and we convert to mU/min
 ubo = ubo * U2mU*min2h;        % Insulin measurements are given in U/h and we convert to mU/min
+
+
 %% Time format conversion
 
 % We loop over the cell array and convert it to a string such that it gets
@@ -51,14 +59,15 @@ end
 for i = 1:length(time_temp_insulin_carb)
     t2(i)= datetime(time_temp_insulin_carb{i},'InputFormat','dd/MM/yyyy HH:mm');
 end
-%% GRID - temp
-delta_G        = 15;                 % From article
-tau            = 6;                  % From the article
-Gmin           = [130 1.5 1.6];      % For meal under 50 considered
 
-% making vector to numerical span between measurements
+%% GRID - temp
+
+% Preparing the time vector for GRID
+
 TT = zeros(1,length(t));
 TT(1) = 0;
+
+% Making vector to numerical span between measurements
 for i = 1:length(t)-1
     temp_time1 = datevec(t(i));
     temp_time2 = datevec(t(i+1));
@@ -71,18 +80,29 @@ for i = 1:length(t)-1
 end
 
 % step size between control steps
+
 Ts = zeros(1,length(TT));
+
 for k = 1:length(TT)-1
-    Ts(k) = TT(k+1)-TT(k);
+    Ts(k) = TT(k+1)-TT(k); % This is the final vector to use
 end
 
+% Initializing for the GRID algorithm
+delta_G        = 15;                 % From article
+tau            = 6;                  % From the article
+Gmin           = [130 1.5 1.6];      % For meal under 50 considered
+
+% Computing the GRID algorithm
 D_detected = GRIDalgorithm_mealdetection2(CGM,Gmin,tau,delta_G,TT,Ts);
 
 % The total amount of detected meals 
 number_detectedmeals = sum(D_detected);
 
+% Printing the number of detected meals
 fprintf('number of detected meals: %d\n',number_detectedmeals);
-%% % Plots over the 4 measurements, G, ubo, uba and D for the whole time frame from january-june
+
+%% Plots over the 4 measurements, G, ubo, uba and D for the whole time frame from january-june
+
 %Plot glucose concentration from january till June
 %subplot(4,1,1)
 figure;
